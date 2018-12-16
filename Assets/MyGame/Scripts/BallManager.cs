a69e2b27fd0d0791d14b827f58dae9c5eac5db5e
smeerws <hello@xrux.at>
2018-12-16 20:24:25 +0100

    Refactor magic strings, rewrite instanciate gameManager

--- Assets/MyGame/Scripts/BallManager.cs
+++ Assets/MyGame/Scripts/BallManager.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class BallManager : MonoBehaviour {
 
+    private const string TAGPLAYER = "Player";
+    private const string TAGCOLLIDER = "Collider";
+
     GameManager gameManager;
 
     private void Start()
     {
-        
-        gameManager = (GameManager)FindObjectOfType(typeof(GameManager));   
-        if(gameManager == null)
-        {
-            Debug.Log("game manager == null");
-        }
+        gameManager = FindObjectOfType<GameManager>();   
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if(collision.tag == TAGPLAYER)
         {
             if(gameManager!= null)
             {
                 gameManager.SetPlayerHit(true);
             }
-            
         }
-        else if (collision.tag == "Collider")
+        else if (collision.tag == TAGCOLLIDER)
         {
             Destroy(gameObject);
         }
